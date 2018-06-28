17610192301a3a83af794c562798e912331002ee
Ben Schreiber <benobi1995@gmail.com>
2018-06-28 10:22:42 -0600

    Dynamically create Waiting List for page

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template
+
+from players import PlayerList
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+    """
+    Default page for webpage, which loads player data from a JSON file,
+    and outputs them to a waiting list, which is displayed on the page.
+    """
+    playerList = PlayerList.fromJSON()
+
+    return render_template('index.html', waitingList=playerList.toHTML())
 
 
 if __name__ == "__main__":
+    """
+    When calling this file in the command line, run the application
+    """
     app.run()
